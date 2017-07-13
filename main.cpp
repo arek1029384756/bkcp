3410c96501c0a2bd765847fc4820ead7ee343491
Arek <arek@arek>
2017-07-13 23:24:15 +0200

    App class added

--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 #include "NsStatusReader.hpp"
 
-int main(int argc, char** argv) {
-    try {
-        auto ns = status_reader::NsStatusReader::getInstance();
-        auto lf = ns->getFileList((argc > 1) ? argv[1] : "<empty>");
-        auto dest = file_ops::FileOps::makeDir((argc > 2) ? argv[2] : "./");
-        for(auto x : lf) {
-            file_ops::FileOps::copyFile(x, dest);
+namespace {
+    class App {
+        int m_argc;
+        char** m_argv;
+
+        public:
+        App(int argc, char** argv)
+            : m_argc(argc), m_argv(argv) {}
+
+        int run() {
+            try {
+                auto ns = status_reader::NsStatusReader::getInstance();
+                auto lf = ns->getFileList((m_argc > 1) ? m_argv[1] : "<empty>");
+                auto dest = file_ops::FileOps::makeDir((m_argc > 2) ? m_argv[2] : "./");
+                for(auto x : lf) {
+                    file_ops::FileOps::copyFile(x, dest);
+                }
+            } catch(const std::runtime_error& e) {
+                std::cout << e.what() << std::endl;
+                return EXIT_FAILURE;
+            } catch(...) {
+                std::cout << "Unknown exception! Something really bad happened"
+                    << std::endl;
+                return EXIT_FAILURE;
+            }
+            return 0;
         }
-    } catch(const std::runtime_error& e) {
-            std::cout << e.what() << std::endl;
-            return EXIT_FAILURE;
-    } catch(...) {
-            std::cout << "Unknown exception! Something really bad happened" 
-                        << std::endl;
-            return EXIT_FAILURE;
-    }
-    return 0;
+    };
+}
+
+int main(int argc, char** argv) {
+    App a(argc, argv);
+    return a.run();
 }
 
